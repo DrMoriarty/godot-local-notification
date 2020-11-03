ccb81309d951bede6c6e33a7e4a1d73c44be360e
Vasiliy Makarov <drmoriarty.0@gmail.com>
2020-11-03 13:34:31 +0300

    fix for android, 0.0.3

--- android-plugin/src/main/java/ru/mobilap/localnotification/LocalNotificationReceiver.java
+++ android-plugin/src/main/java/ru/mobilap/localnotification/LocalNotificationReceiver.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
 import android.net.Uri;
@@ -36,8 +37,16 @@ public class LocalNotificationReceiver extends BroadcastReceiver {
             manager.createNotificationChannel(notificationChannel) ;
         }
 
-        Intent intent2 = new Intent(context, Godot.class);
-        intent2.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        Class appClass = null;
+        try {
+            appClass = Class.forName("com.godot.game.GodotApp");
+        } catch (ClassNotFoundException e) {
+            // app not found, do nothing
+            return;
+        }
+        
+        Intent intent2 = new Intent(context, appClass);
+        intent2.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
 
         int iconID = context.getResources().getIdentifier("icon", "mipmap", context.getPackageName());
@@ -49,11 +58,15 @@ public class LocalNotificationReceiver extends BroadcastReceiver {
         builder.setContentText(message);
         builder.setSmallIcon(iconID);
         builder.setLargeIcon(largeIcon);
+        builder.setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE);
         builder.setTicker(message);
         builder.setAutoCancel(true);
         builder.setDefaults(Notification.DEFAULT_ALL);
+        builder.setColorized(true);
+        builder.setColor(Color.RED);
         builder.setContentIntent(pendingIntent);
         builder.setNumber(1);
+        //builder.addAction();
         //builder.setSound(Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.bomb3));
         builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
         //long[] pattern = {10,10};
